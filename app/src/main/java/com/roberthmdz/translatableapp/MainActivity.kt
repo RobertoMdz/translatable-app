b84bcf6326d2a1ccc401bf5eebd3025685163e02
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-11-03 22:00:12 -0500

    Initial texts to translate

--- app/src/main/java/com/roberthmdz/translatableapp/MainActivity.kt
+++ app/src/main/java/com/roberthmdz/translatableapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.roberthmdz.translatableapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.roberthmdz.translatableapp.ui.theme.TranslatableAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +18,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TranslatableAppTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+              Column(
+                  modifier = Modifier
+                      .fillMaxSize()
+                      .padding(16.dp)
+              ) {
+
+                  Text(text = "Hello world")
+                  Spacer(modifier = Modifier.height(16.dp))
+                  Text(text = "This text should be translated")
+                  
+
+
+              }
             }
         }
     }
